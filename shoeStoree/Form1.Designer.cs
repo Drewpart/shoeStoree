32263f28570fd34231df0cf409dbf6e26ada5f35
Drew Partridge <andrpart669@ed.amdsb.ca>
2022-10-04 11:09:43 -0400

    Add project files.

---
+++ shoeStoree/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace shoeStoree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.jordanLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Jordan1Label = new System.Windows.Forms.Label();
+            this.Jordan6Label = new System.Windows.Forms.Label();
+            this.jordan4Input = new System.Windows.Forms.TextBox();
+            this.Jordan6Input = new System.Windows.Forms.TextBox();
+            this.jordan1Input = new System.Windows.Forms.TextBox();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.TLabel = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ChangeOutput = new System.Windows.Forms.Label();
+            this.reciptLabel = new System.Windows.Forms.Label();
+            this.reciptButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jordanLabel
+            // 
+            this.jordanLabel.BackColor = System.Drawing.Color.Red;
+            this.jordanLabel.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jordanLabel.Location = new System.Drawing.Point(12, 47);
+            this.jordanLabel.Name = "jordanLabel";
+            this.jordanLabel.Size = new System.Drawing.Size(160, 31);
+            this.jordanLabel.TabIndex = 0;
+            this.jordanLabel.Text = "Jordan 4 Taupe Haze";
+            this.jordanLabel.Click += new System.EventHandler(this.jordanLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // Jordan1Label
+            // 
+            this.Jordan1Label.BackColor = System.Drawing.Color.Red;
+            this.Jordan1Label.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jordan1Label.Location = new System.Drawing.Point(12, 96);
+            this.Jordan1Label.Name = "Jordan1Label";
+            this.Jordan1Label.Size = new System.Drawing.Size(160, 31);
+            this.Jordan1Label.TabIndex = 2;
+            this.Jordan1Label.Text = "Jordan 1";
+            this.Jordan1Label.Click += new System.EventHandler(this.Jordan1Label_Click);
+            // 
+            // Jordan6Label
+            // 
+            this.Jordan6Label.BackColor = System.Drawing.Color.Red;
+            this.Jordan6Label.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jordan6Label.Location = new System.Drawing.Point(12, 153);
+            this.Jordan6Label.Name = "Jordan6Label";
+            this.Jordan6Label.Size = new System.Drawing.Size(160, 31);
+            this.Jordan6Label.TabIndex = 3;
+            this.Jordan6Label.Text = "Jordan 6";
+            this.Jordan6Label.Click += new System.EventHandler(this.Jordan6Label_Click);
+            // 
+            // jordan4Input
+            // 
+            this.jordan4Input.Location = new System.Drawing.Point(178, 58);
+            this.jordan4Input.Name = "jordan4Input";
+            this.jordan4Input.Size = new System.Drawing.Size(89, 20);
+            this.jordan4Input.TabIndex = 4;
+            // 
+            // Jordan6Input
+            // 
+            this.Jordan6Input.Location = new System.Drawing.Point(178, 164);
+            this.Jordan6Input.Name = "Jordan6Input";
+            this.Jordan6Input.Size = new System.Drawing.Size(89, 20);
+            this.Jordan6Input.TabIndex = 5;
+            // 
+            // jordan1Input
+            // 
+            this.jordan1Input.Location = new System.Drawing.Point(178, 107);
+            this.jordan1Input.Name = "jordan1Input";
+            this.jordan1Input.Size = new System.Drawing.Size(89, 20);
+            this.jordan1Input.TabIndex = 6;
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.AutoSize = true;
+            this.subTotalLabel.BackColor = System.Drawing.Color.Red;
+            this.subTotalLabel.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalLabel.Location = new System.Drawing.Point(12, 228);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(107, 22);
+            this.subTotalLabel.TabIndex = 7;
+            this.subTotalLabel.Text = "SubTotal";
+            this.subTotalLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.BackColor = System.Drawing.Color.Red;
+            this.totalLabel.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(12, 308);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(68, 22);
+            this.totalLabel.TabIndex = 8;
+            this.totalLabel.Text = "Total";
+            this.totalLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.BackColor = System.Drawing.Color.Red;
+            this.taxLabel.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(12, 266);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(50, 22);
+            this.taxLabel.TabIndex = 9;
+            this.taxLabel.Text = "Tax";
+            this.taxLabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Image = global::shoeStoree.Properties.Resources.shoeIconJordan4RetroRed;
+            this.label5.Location = new System.Drawing.Point(12, 344);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(255, 47);
+            this.label5.TabIndex = 10;
+            // 
+            // TenderedLabel
+            // 
+            this.TenderedLabel.AutoSize = true;
+            this.TenderedLabel.BackColor = System.Drawing.Color.Red;
+            this.TenderedLabel.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenderedLabel.Location = new System.Drawing.Point(11, 405);
+            this.TenderedLabel.Name = "TenderedLabel";
+            this.TenderedLabel.Size = new System.Drawing.Size(108, 22);
+            this.TenderedLabel.TabIndex = 11;
+            this.TenderedLabel.Text = "Tendered";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(151, 407);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(61, 20);
+            this.tenderedInput.TabIndex = 12;
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.BackColor = System.Drawing.Color.Red;
+            this.subtotalOutput.Location = new System.Drawing.Point(151, 228);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(116, 23);
+            this.subtotalOutput.TabIndex = 13;
+            // 
+            // TLabel
+            // 
+            this.TLabel.BackColor = System.Drawing.Color.Red;
+            this.TLabel.Location = new System.Drawing.Point(151, 307);
+            this.TLabel.Name = "TLabel";
+            this.TLabel.Size = new System.Drawing.Size(116, 23);
+            this.TLabel.TabIndex = 14;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.BackColor = System.Drawing.Color.Red;
+            this.taxOutput.Location = new System.Drawing.Point(151, 265);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(116, 23);
+            this.taxOutput.TabIndex = 15;
+            // 
+            // totalButton
+            // 
+            this.totalButton.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalButton.Location = new System.Drawing.Point(12, 202);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(200, 23);
+            this.totalButton.TabIndex = 16;
+            this.totalButton.Text = "Calculate Total";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 22);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Change";
+            // 
+            // ChangeOutput
+            // 
+            this.ChangeOutput.BackColor = System.Drawing.Color.Red;
+            this.ChangeOutput.Location = new System.Drawing.Point(151, 458);
+            this.ChangeOutput.Name = "ChangeOutput";
+            this.ChangeOutput.Size = new System.Drawing.Size(116, 23);
+            this.ChangeOutput.TabIndex = 19;
+            // 
+            // reciptLabel
+            // 
+            this.reciptLabel.ForeColor = System.Drawing.Color.White;
+            this.reciptLabel.Location = new System.Drawing.Point(351, 47);
+            this.reciptLabel.Name = "reciptLabel";
+            this.reciptLabel.Size = new System.Drawing.Size(266, 283);
+            this.reciptLabel.TabIndex = 20;
+            // 
+            // reciptButton
+            // 
+            this.reciptButton.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reciptButton.Location = new System.Drawing.Point(354, 333);
+            this.reciptButton.Name = "reciptButton";
+            this.reciptButton.Size = new System.Drawing.Size(263, 23);
+            this.reciptButton.TabIndex = 21;
+            this.reciptButton.Text = "Print Recipt";
+            this.reciptButton.UseVisualStyleBackColor = true;
+            this.reciptButton.Click += new System.EventHandler(this.reciptButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(496, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 49);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "New Order";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(365, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 49);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Cancel Order";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(618, 487);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.reciptButton);
+            this.Controls.Add(this.reciptLabel);
+            this.Controls.Add(this.ChangeOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.TLabel);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.TenderedLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.jordan1Input);
+            this.Controls.Add(this.Jordan6Input);
+            this.Controls.Add(this.jordan4Input);
+            this.Controls.Add(this.Jordan6Label);
+            this.Controls.Add(this.Jordan1Label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.jordanLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simple Shoes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label jordanLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Jordan1Label;
+        private System.Windows.Forms.Label Jordan6Label;
+        private System.Windows.Forms.TextBox jordan4Input;
+        private System.Windows.Forms.TextBox Jordan6Input;
+        private System.Windows.Forms.TextBox jordan1Input;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label TenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label TLabel;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ChangeOutput;
+        private System.Windows.Forms.Label reciptLabel;
+        private System.Windows.Forms.Button reciptButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ shoeStoree/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace shoeStoree
+{
+    public partial class Form1 : Form
+    {
+
+
+        double jordan4Price = 397.21;
+        double jordan1Price = 218;
+        double jordan6Price = 192;
+
+        //double taxAmmount = 1.13;
+        //double total = 0;
+        //double tendered = 0;
+        //double change = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Jordan6Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Jordan1Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void jordanLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int jordan4 = Convert.ToInt32(jordan4Input.Text);
+                int jordan1 = Convert.ToInt32(jordan1Input.Text);
+                int jordan6 = Convert.ToInt32(Jordan6Input.Text);
+
+
+                double totalJordan4 = jordan4 * jordan4Price;
+                double totalJordan1 = jordan1 * jordan1Price;
+                double totalJordan6 = jordan6 * jordan6Price;
+                double subTotal = totalJordan4 + totalJordan1 + totalJordan6;
+                double taxRate = 1.13;
+                double total = subTotal * taxRate;
+                double taxAmmount = total - subTotal;
+
+
+
+                // doing calculations
+
+                subtotalOutput.Text = $"Sub Total: {subTotal.ToString("0.00")}";
+                TLabel.Text = $"Total: {total.ToString("0.00")}";
+              taxOutput.Text = $" Tax: {taxAmmount.ToString("0.00")}";
+
+                reciptLabel.Text = "SWEET!";
+
+
+            }
+            catch
+            {
+                reciptLabel.Text = "Please Enter Enough Money";
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int jordan4 = Convert.ToInt32(jordan4Input.Text);
+            int jordan1 = Convert.ToInt32(jordan1Input.Text);
+            int jordan6 = Convert.ToInt32(Jordan6Input.Text);
+
+            double totalJordan4 = jordan4 * jordan4Price;
+            double totalJordan1 = jordan1 * jordan1Price;
+            double totalJordan6 = jordan6 * jordan6Price;
+            double subTotal = totalJordan4 + totalJordan1 + totalJordan6;
+            double taxRate = 1.13;
+            double total = subTotal * taxRate;
+            double taxAmmount = total - subTotal;
+            double tendered = Convert.ToDouble(tenderedInput.Text);
+            double change = tendered - total;
+            ChangeOutput.Text = $"Change: {change.ToString("0.00")}";
+            reciptLabel.Text = $"Your change will be ${change.ToString("0.00")}";
+            reciptLabel.Text += $"  Would you like a copy of the Receipt?";
+
+        }
+
+        private void reciptButton_Click(object sender, EventArgs e)
+        {
+            int jordan4 = Convert.ToInt32(jordan4Input.Text);
+            int jordan1 = Convert.ToInt32(jordan1Input.Text);
+            int jordan6 = Convert.ToInt32(Jordan6Input.Text);
+
+            double totalJordan4 = jordan4 * jordan4Price;
+            double totalJordan1 = jordan1 * jordan1Price;
+            double totalJordan6 = jordan6 * jordan6Price;
+            double subTotal = totalJordan4 + totalJordan1 + totalJordan6;
+            double taxRate = 1.13;
+            double total = subTotal * taxRate;
+            double taxAmmount = total - subTotal;
+            double tendered = Convert.ToDouble(tenderedInput.Text);
+            double change = tendered - total;
+            reciptLabel.BackColor = Color.Gray;
+            reciptLabel.Text = $"          Simple Shoes\n\n order number 5760\n october 4, 2022\n\n Jordan 1's x{jordan1}            @{jordan1Price}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n Jordan 6's{jordan6}              @{jordan6Price}";
+            reciptLabel.Text += $"\n Jordan 4's x{jordan4}           @{jordan4Price}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n\n Sub Total:                  {subTotal}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n Tax:                           {taxAmmount.ToString("0.00")}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n Total:                         {total}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n\n Tendered:                  {tendered.ToString("0.00")}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n Change:                     {change.ToString("0.00")}";
+            Refresh();
+            Thread.Sleep(15);
+            reciptLabel.Text += $"\n\n Have A Great Day!\n Thank you for shopping.";
+
+
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            jordan1Input.Text = "";
+            jordan4Input.Text = "";
+            Jordan6Input.Text = "";
+            reciptLabel.Text = "Welcome!";
+            taxLabel.Text = "Tax:";
+            totalLabel.Text = "Total:";
+            subTotalLabel.Text = "Sub Total:";
+            tenderedInput.Text = "";
+            reciptLabel.Text = "";
+            ChangeOutput.Text = "Change:";
+            taxLabel.Text = "";
+            TLabel.Text = "";
+            subTotalLabel.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            reciptLabel.Text = $"you have canceled your order, have a great day";
+            jordan1Input.Text = "";
+            jordan4Input.Text = "";
+            Jordan6Input.Text = "";
+            reciptLabel.Text = "Welcome!";
+            taxLabel.Text = "Tax:";
+            totalLabel.Text = "Total:";
+            subTotalLabel.Text = "Sub Total:";
+            tenderedInput.Text = "";
+            reciptLabel.Text = "";
+            ChangeOutput.Text = "Change:";
+            Application.Restart();
+        }
+    }
+}
